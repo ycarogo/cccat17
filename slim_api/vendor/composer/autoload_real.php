094ec1a1034470f93d5df34170d9a238de79a742
Ycaro Gonçalves <ycarogoncalves@gmail.com>
2025-03-17 22:14:01 -0300

    ajustes de dockerfile

--- slim_api/vendor/composer/autoload_real.php
+++ slim_api/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit7dcdc5dd1f490916ed0def183ae57c40
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit7dcdc5dd1f490916ed0def183ae57c40', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit7dcdc5dd1f490916ed0def183ae57c40', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit7dcdc5dd1f490916ed0def183ae57c40
         call_user_func(\Composer\Autoload\ComposerStaticInit7dcdc5dd1f490916ed0def183ae57c40::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit7dcdc5dd1f490916ed0def183ae57c40::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
